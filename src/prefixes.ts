96af889b7b65f739d8f173339657b69616639e2e
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-06-09 15:57:28 +0200

    feat: prefixes augmentation

--- src/prefixes.ts
+++ src/prefixes.ts
@@ -1,5 +1,15 @@
-export default {
+const prefixes = {
   code: 'https://code.described.at/',
   cube: 'https://cube.link/',
   pipeline: 'https://pipeline.described.at/'
 }
+
+type LocalPrefixes = typeof prefixes
+
+declare module '@zazuko/rdf-vocabularies/prefixes' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Prefixes extends LocalPrefixes {
+  }
+}
+
+export default prefixes
