c95cacd80a95281dfacb6b1cf2305bad5b9968ef
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-07-06 12:43:34 +0200

    fix: terms lost from builders

---
+++ .changeset/shiny-dingos-train.md
@@ -0,0 +1,5 @@
+---
+"@zazuko/vocabulary-extras": patch
+---
+
+8309fca9 inadvertently caused all members to disappear from builder modules
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import './register'
+
 export { default as prefixes } from './prefixes'
 export { vocabularies, loadFile } from './vocabularies'
